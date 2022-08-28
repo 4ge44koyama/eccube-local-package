aa6b01bb78c1972ec2ee019e5fe0ee913ebb4f23
code_gazer <60380497+4ge44koyama@users.noreply.github.com>
2022-08-28 09:59:23 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -56,6 +56,7 @@ git clone -b 4.1 https://github.com/EC-CUBE/ec-cube /var/www/ec-cube
 
 # Composerのインストール
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+# ここのハッシュ値は都度確認する(https://getcomposer.org/download/)
 php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
